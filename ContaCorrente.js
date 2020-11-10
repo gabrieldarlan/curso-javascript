af19cb29bc61e356b95766352795ad100911aa68
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-11-10 14:53:46 -0300

    cap03 completo

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,45 +1,16 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js"
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
-
-    get cliente() {
-        return this._cliente;
-    }
-  
-    set cliente(value) {
-        if (value instanceof Cliente) {
-            this._cliente = value;
-        }
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
     constructor(cliente, agencia) {
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
     }
 
-    depositar(valor) {
-        const valorNegativo = valor <= 0;
-        if (valorNegativo) return valor
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }
-    
+        
 }
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import { Cliente } from "./Cliente.js";
-import { Conta } from "./Conta.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Gabriel", 39562057828);
 
-const contaCorrenteGabriel = new Conta(0, cliente1, 1001);
+const contaCorrenteGabriel = new ContaCorrente(cliente1, 1001);
 contaCorrenteGabriel.depositar(500)
 contaCorrenteGabriel.sacar(100)
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
-console.log(contaPoupanca);
-console.log(contaCorrenteGabriel);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
+// console.log(contaPoupanca);
+
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+console.log(contaSalario);
